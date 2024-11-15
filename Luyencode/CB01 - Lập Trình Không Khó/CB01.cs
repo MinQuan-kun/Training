d34842830b0b5ea2791469ef6ffbde378dd43639
MinQuan-kun <ikkun2705@gmail.com>
2024-11-15 21:13:01 +0700

    CB1

---
+++ Luyencode/CB01 - Lập Trình Không Khó/CB01.cs
@@ -0,0 +1,11 @@
+using System;
+namespace CB
+{
+    public class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.Write("Lap Trinh Khong Kho!");
+        }
+    }
+}
