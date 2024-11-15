c79b4d80decce4fbe35c3cc652dc49e0da7184b5
MinQuan-kun <ikkun2705@gmail.com>
2024-11-15 21:52:35 +0700

    DK01

---
+++ Luyencode/DK01 - Tìm số lớn nhất/DK01.cs
@@ -0,0 +1,17 @@
+using System;
+namespace CB8
+{
+    public class Program
+    {
+        static void Main(string[]args)
+        {
+            string []line = Console.ReadLine().Split(' ');
+            int a = int.Parse(line[0]);
+            int b = int.Parse(line[1]);
+            int max = a;
+            if(max < b)
+                max = b;
+            Console.Write(max);
+        }
+    }
+}
