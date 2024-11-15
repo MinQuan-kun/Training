640e871b5188f94af6f25d469a38e56eaad079a8
MinQuan-kun <ikkun2705@gmail.com>
2024-11-15 21:15:10 +0700

    CB3 c# fix

--- Luyencode/CB03 - Tính tổng của 3 số nguyên/CB03.cs
+++ Luyencode/CB03 - Tính tổng của 3 số nguyên/CB03.cs
@@ -6,9 +6,9 @@ namespace Tinhtong
         static void Main(string[]args)
         {
             string []line = Console.ReadLine().Split(' ');
-            int a = int.Parse(input[0]);
-            int b = int.Parse(input[0]);
-            int c = int.Parse(input[0]);
+            int a = int.Parse(line[0]);
+            int b = int.Parse(line[1]);
+            int c = int.Parse(line[2]);
             Console.Write(a + b + c);
         }
     }
