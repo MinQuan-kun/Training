7c823c3be85fe74ecccea606f84add0d4e3daed4
MinQuan-kun <ikkun2705@gmail.com>
2024-12-07 20:25:51 +0700

    LTCB-31

---
+++ Upcoder/Other/Kiem_Tra_So_NgTo/LTCB-31.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool CheckSNT(long long n) {
+    if (n < 2)
+        return false;
+    if (n == 2 || n == 3)
+        return true;
+    for (long long i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    long long n;
+    while (cin >> n) {
+        if (CheckSNT(n))
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+    return 0;
+}
