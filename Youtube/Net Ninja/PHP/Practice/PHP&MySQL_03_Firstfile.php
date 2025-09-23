cdb04522f81528faa54f35536b71d7b380649d4b
Nguyễn Hữu Minh Quân <ikkun2705@gmail.com>
2025-09-23 21:15:08 +0700

    First day learn Php

---
+++ Youtube/Net Ninja/PHP/Practice/PHP&MySQL_03_Firstfile.php
@@ -0,0 +1,26 @@
+<?php
+
+// echo "Hello, welcome to the PHP & MySQL learning page!";
+
+// echo "This page is under construction. Please check back later.";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first PHP file</title>
+</head>
+<body>
+
+    <h1>
+        <?php 
+        echo "Hello, welcome to the PHP & MySQL learning page!";
+    
+        ?>
+    </h1>
+
+
+</body>
+</html>
