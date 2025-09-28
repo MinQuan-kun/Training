8c479d488a9129504595d2aedcf1c83880e09509
Nguyễn Hữu Minh Quân <ikkun2705@gmail.com>
2025-09-28 11:05:53 +0700

    Bai 10

---
+++ Youtube/Net Ninja/PHP/Practice/PHP&MySQL_10_Booleans & Comparisons.php
@@ -0,0 +1,39 @@
+<?php 
+
+// comparisons booleas
+// echo true;
+// echo false;
+
+echo 5 < 10;
+echo "<br>";
+echo 5 > 10;
+echo "<br>";
+echo 10 == 10;
+echo "<br>";
+echo 10 != 10;
+
+// string
+echo 'ikkun' < 'ikkuna';
+echo "<br>";
+echo 'ikkuna' < 'ikkunb';
+echo 5 == '5';
+echo "<br>";
+echo 5 === '5'; // strict comparison
+echo true == "1";
+echo false == "";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP & MySQL</title>
+
+</head>
+<body>
+
+</body>
+</html>
